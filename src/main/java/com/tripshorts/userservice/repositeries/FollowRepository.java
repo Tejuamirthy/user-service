c621f0b65a77961df1861af0d2e818dd4c1ad3a7
tejuamirthi <tejuamirthi@gmail.com>
2020-03-11 23:59:36 +0530

    Get followers todo native query

--- src/main/java/com/tripshorts/userservice/repositeries/FollowRepository.java
+++ src/main/java/com/tripshorts/userservice/repositeries/FollowRepository.java
@@ -4,12 +4,16 @@ import com.tripshorts.userservice.entity.FollowersTable;
 import com.tripshorts.userservice.entity.UserEntity;
 import com.tripshorts.userservice.model.UserDTO;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
 @Repository
 public interface FollowRepository extends JpaRepository<FollowersTable, FollowersTable.ForeignCompositeKey> {
-    List<Long> findByUserEntity(UserEntity userEntity);
-    List<Long> findByUserFollowerEntity(UserEntity userFollowerEntity);
+    @Query(value = "SELECT * FROM followers f where f.id = :uId ;", nativeQuery = true)
+    List<FollowersTable> findByUserEntity(@Param("uId") int l);
+//    List<Long> findByUserFollowerEntity(UserEntity userFollowerEntity);
+
 }
