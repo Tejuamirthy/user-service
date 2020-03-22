65cba19b156cda4fa39356043f449402fd94c541
tejuamirthi <tejuamirthi@gmail.com>
2020-03-23 00:19:44 +0530

    Updated method name and added new methods based on jpa spring data processing

--- src/main/java/com/tripshorts/userservice/repositeries/FollowRepository.java
+++ src/main/java/com/tripshorts/userservice/repositeries/FollowRepository.java
@@ -15,6 +15,9 @@ public interface FollowRepository extends JpaRepository<FollowersTable, Long> {
 
     // To get the followers with user id as "id"
     @Query(value = "SELECT * FROM followers f where f.from_id = :uId ;", nativeQuery = true)
-    List<FollowersTable> findById(@Param("uId") long id);
+    List<FollowersTable> findFollowersById(@Param("uId") long id);
 
+
+    List<FollowersTable> findByUserEntity(UserEntity userEntity);
+    List<FollowersTable> findByUserFollowerEntity(UserEntity userEntity);
 }
