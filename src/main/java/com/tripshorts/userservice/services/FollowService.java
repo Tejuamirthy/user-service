ecfe98d7d8722996eab94f40bb0a83c8901722a0
tejuamirthi <tejuamirthi@gmail.com>
2020-04-27 12:03:03 +0530

    Follow service and implementation commit

--- src/main/java/com/tripshorts/userservice/services/FollowService.java
+++ src/main/java/com/tripshorts/userservice/services/FollowService.java
@@ -1,16 +1,14 @@
 package com.tripshorts.userservice.services;
 
 
-import com.tripshorts.userservice.exceptions.UserNotFound;
+import com.tripshorts.userservice.exceptions.UserException;
 import com.tripshorts.userservice.model.UserDTO;
-
-import java.security.Principal;
 import java.util.List;
 
 public interface FollowService {
-    UserDTO follow(String username, String currUsername) throws UserNotFound;
-    UserDTO unfollow(String username, String currUsername) throws UserNotFound;
-    List<UserDTO> getUserFollowers(String username) throws UserNotFound;
-    List<UserDTO> getUserFollowing(String username) throws UserNotFound;
+    UserDTO follow(String username, String currUserId) throws UserException;
+    UserDTO unfollow(String username, String currUserId) throws UserException;
+    List<UserDTO> getUserFollowers(String userId) throws UserException;
+    List<UserDTO> getUserFollowing(String userId) throws UserException;
 //    List<Long> getUserById(int id);
 }
