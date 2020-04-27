48ec366fc3c303cbfb697cb777166187d223f827
tejuamirthi <tejuamirthi@gmail.com>
2020-04-27 12:04:15 +0530

    Commented login controller and implementation files

--- src/main/java/com/tripshorts/userservice/services/implementation/LoginServiceImplementation.java
+++ src/main/java/com/tripshorts/userservice/services/implementation/LoginServiceImplementation.java
@@ -1,121 +1,121 @@
-package com.tripshorts.userservice.services.implementation;
-
-import com.tripshorts.userservice.entity.FollowersTable;
-import com.tripshorts.userservice.entity.UserEntity;
-import com.tripshorts.userservice.exceptions.UserAlreadyExists;
-import com.tripshorts.userservice.exceptions.UserNotFound;
-import com.tripshorts.userservice.model.LoginRequest;
-import com.tripshorts.userservice.model.UserDTO;
-import com.tripshorts.userservice.repositeries.FollowRepository;
-import com.tripshorts.userservice.repositeries.UserRepository;
-import com.tripshorts.userservice.services.LoginService;
-import org.springframework.beans.BeanUtils;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.stereotype.Service;
-
-
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Optional;
-
-@Service
-public class LoginServiceImplementation implements LoginService {
-
-    @Autowired
-    private UserRepository userRepository;
-
-    @Autowired
-    private FollowRepository followRepository;
-
-    @Autowired
-    private PasswordEncoder passwordEncoder;
-
-    @Override
-    public UserDTO findUserByUsername(String username) throws UserNotFound {
-        Optional<UserEntity> optionalUserEntity = userRepository.findByUsername(username);
-        UserDTO userDTO = null;
-        if(optionalUserEntity.isPresent()) {
-            userDTO = new UserDTO();
-            BeanUtils.copyProperties(optionalUserEntity.get(), userDTO);
-            return userDTO;
-        }
-        throw new UserNotFound("User Not Found with given Id");
-    }
-
-    @Override
-    public UserDTO findUserByEmail(String email) throws UserNotFound {
-        Optional<UserEntity> optionalUserEntity = userRepository.findByEmail(email);
-        UserDTO userDTO = new UserDTO();
-        if(!optionalUserEntity.isPresent())
-            throw new UserNotFound("User not found with email");
-        BeanUtils.copyProperties(optionalUserEntity, userDTO);
-        return userDTO;
-    }
-
-    @Override
-    public UserDTO findUserById(Long id) throws UserNotFound {
-        Optional<UserEntity> optionalUserEntity = userRepository.findById(id);
-        UserDTO userDTO = null;
-        if(optionalUserEntity.isPresent()) {
-            userDTO = new UserDTO();
-            BeanUtils.copyProperties(optionalUserEntity.get(), userDTO);
-            return userDTO;
-        }
-        throw new UserNotFound("User Not Found with given Id");
-    }
-
-    @Override
-    public UserDTO login(LoginRequest loginRequest) throws UserNotFound {
-        return null;
-    }
-
-    @Override
-    public UserDTO signUp(UserDTO userDTO) throws UserAlreadyExists {
-
-        try {
-            findUserByUsername(userDTO.getUsername());
-            throw new UserAlreadyExists("Username already Exists");
-        } catch (UserNotFound userNotFound) {
-
-            try {
-                findUserByEmail(userDTO.getEmail());
-                throw new UserAlreadyExists("Email already Exists");
-            } catch (UserNotFound notFound) {
-                UserEntity userEntity = new UserEntity();
-                BeanUtils.copyProperties(userDTO, userEntity);
-                userEntity.setPassword(passwordEncoder.encode(userEntity.getPassword()));
-                userRepository.save(userEntity);
-                return userDTO;
-            }
-
-        }
-    }
-
-
-    @Override
-    public UserDTO updateUserDetails(UserDTO userDTO) throws UserNotFound {
-        UserEntity userEntity = userRepository.findByUsername(userDTO.getUsername()).orElse(null);
-        if(userEntity != null) {
-            Long id = userEntity.getId();
-            BeanUtils.copyProperties(userDTO, userEntity);
-            userEntity.setPassword(passwordEncoder.encode(userEntity.getPassword()));
-            userEntity.setId(id);
-            userRepository.save(userEntity);
-            return  userDTO;
-        }
-        throw new UserNotFound("Given user not found");
-    }
-
-    @Override
-    public List<String> getUserRoles(String username) throws UserNotFound {
-        UserEntity userEntity = userRepository.findByUsername(username).orElse(null);
-        if(userEntity == null) {
-            throw new UserNotFound("User name not found");
-        }
-        List<String> stringList = new ArrayList<>();
-        userEntity.getRoles().forEach(role -> stringList.add(role.getRole()));
-        return stringList;
-    }
-
-}
+//package com.tripshorts.userservice.services.implementation;
+//
+//import com.tripshorts.userservice.entity.FollowersTable;
+//import com.tripshorts.userservice.entity.UserEntity;
+//import com.tripshorts.userservice.exceptions.UserAlreadyExists;
+//import com.tripshorts.userservice.exceptions.UserNotFound;
+//import com.tripshorts.userservice.model.LoginRequest;
+//import com.tripshorts.userservice.model.UserDTO;
+//import com.tripshorts.userservice.repositeries.FollowRepository;
+//import com.tripshorts.userservice.repositeries.UserRepository;
+//import com.tripshorts.userservice.services.LoginService;
+//import org.springframework.beans.BeanUtils;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.security.crypto.password.PasswordEncoder;
+//import org.springframework.stereotype.Service;
+//
+//
+//import java.util.ArrayList;
+//import java.util.List;
+//import java.util.Optional;
+//
+//@Service
+//public class LoginServiceImplementation implements LoginService {
+//
+//    @Autowired
+//    private UserRepository userRepository;
+//
+//    @Autowired
+//    private FollowRepository followRepository;
+//
+//    @Autowired
+//    private PasswordEncoder passwordEncoder;
+//
+//    @Override
+//    public UserDTO findUserByUsername(String username) throws UserNotFound {
+//        Optional<UserEntity> optionalUserEntity = userRepository.findByUsername(username);
+//        UserDTO userDTO = null;
+//        if(optionalUserEntity.isPresent()) {
+//            userDTO = new UserDTO();
+//            BeanUtils.copyProperties(optionalUserEntity.get(), userDTO);
+//            return userDTO;
+//        }
+//        throw new UserNotFound("User Not Found with given Id");
+//    }
+//
+//    @Override
+//    public UserDTO findUserByEmail(String email) throws UserNotFound {
+//        Optional<UserEntity> optionalUserEntity = userRepository.findByEmail(email);
+//        UserDTO userDTO = new UserDTO();
+//        if(!optionalUserEntity.isPresent())
+//            throw new UserNotFound("User not found with email");
+//        BeanUtils.copyProperties(optionalUserEntity, userDTO);
+//        return userDTO;
+//    }
+//
+//    @Override
+//    public UserDTO findUserById(Long id) throws UserNotFound {
+//        Optional<UserEntity> optionalUserEntity = userRepository.findById(id);
+//        UserDTO userDTO = null;
+//        if(optionalUserEntity.isPresent()) {
+//            userDTO = new UserDTO();
+//            BeanUtils.copyProperties(optionalUserEntity.get(), userDTO);
+//            return userDTO;
+//        }
+//        throw new UserNotFound("User Not Found with given Id");
+//    }
+//
+//    @Override
+//    public UserDTO login(LoginRequest loginRequest) throws UserNotFound {
+//        return null;
+//    }
+//
+//    @Override
+//    public UserDTO signUp(UserDTO userDTO) throws UserAlreadyExists {
+//
+//        try {
+//            findUserByUsername(userDTO.getUsername());
+//            throw new UserAlreadyExists("Username already Exists");
+//        } catch (UserNotFound userNotFound) {
+//
+//            try {
+//                findUserByEmail(userDTO.getEmail());
+//                throw new UserAlreadyExists("Email already Exists");
+//            } catch (UserNotFound notFound) {
+//                UserEntity userEntity = new UserEntity();
+//                BeanUtils.copyProperties(userDTO, userEntity);
+//                userEntity.setPassword(passwordEncoder.encode(userEntity.getPassword()));
+//                userRepository.save(userEntity);
+//                return userDTO;
+//            }
+//
+//        }
+//    }
+//
+//
+//    @Override
+//    public UserDTO updateUserDetails(UserDTO userDTO) throws UserNotFound {
+//        UserEntity userEntity = userRepository.findByUsername(userDTO.getUsername()).orElse(null);
+//        if(userEntity != null) {
+//            Long id = userEntity.getId();
+//            BeanUtils.copyProperties(userDTO, userEntity);
+//            userEntity.setPassword(passwordEncoder.encode(userEntity.getPassword()));
+//            userEntity.setId(id);
+//            userRepository.save(userEntity);
+//            return  userDTO;
+//        }
+//        throw new UserNotFound("Given user not found");
+//    }
+//
+//    @Override
+//    public List<String> getUserRoles(String username) throws UserNotFound {
+//        UserEntity userEntity = userRepository.findByUsername(username).orElse(null);
+//        if(userEntity == null) {
+//            throw new UserNotFound("User name not found");
+//        }
+//        List<String> stringList = new ArrayList<>();
+//        userEntity.getRoles().forEach(role -> stringList.add(role.getRole()));
+//        return stringList;
+//    }
+//
+//}
